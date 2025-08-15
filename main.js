19ff18c6ab59504fefc00992ea0c8ca6e4ea3227
Ahmed Tarek Hagar <ahmed.tarek.ronaldo@gmail.com>
2025-08-15 18:27:08 +0300

    add functionality

--- main.js
+++ main.js
@@ -1,30 +1,63 @@
-document.querySelector("button").onclick = function(){
-    document.querySelector("textarea").select();
-    document.execCommand('copy');
-}
 
-// function getElement(id) {
-//     return document.getElementById(id);
-// }
-debugger
-var workItemTitle = document.getElementById('workItemTitle');
-var type = document.getElementById('type');
-var area = document.getElementById('area');
-var workItem01 = document.getElementById('workItem01');
+var workItemTitleHTMLElement = document.getElementById('workItemTitleElement');
+var typeHTMLElement = document.getElementById('typeElement');
+var areaHTMLElement = document.getElementById('areaElement');
+var workItem01HTMLElement = document.getElementById('workItem01Element');
 
-var branchName = document.getElementById('branchName').value;
-var commitMessage = document.getElementById('commitMessage').value;
-var pullRequestTitle = document.getElementById('pullRequestTitle').value;
+var branchNameHTMLElement = document.getElementById('branchNameElement');
+var commitMessageHTMLElement = document.getElementById('commitMessageElement');
+var pullRequestTitleHTMLElement = document.getElementById('pullRequestTitleElement');
 
-document.getElementById('generate').addEventListener('click', generate);
+document.getElementById('generateButtonElement').addEventListener('click', generate);
+
+document.getElementById('branchNameCopyButtonElement').addEventListener('click', copyBranchName);
+document.getElementById('commitMessageCopyButtonElement').addEventListener('click', copyCommitMessage);
+document.getElementById('pullRequestTitleCopyButtonElement').addEventListener('click', copyPullRequestTitle);
 
 function generate() {
-    debugger
-    const workItemTitleValue = workItemTitle.value;
-    // const typevalue = this.type.value;
-    // const areavalue = this.area.value;
-    // const workItem01value = this.workItem01.value;
+    const workItemTitleOriginalValue = workItemTitleHTMLElement.value.toLowerCase();
+    const typeOriginalValue = typeHTMLElement.value.toLowerCase();
+    const areaOriginalValue = areaHTMLElement.value.toLowerCase();
+    const workItem01OriginalValue = workItem01HTMLElement.value.toLowerCase();
 
-    const workItemTitleDashed = workItemTitleValue.split(" ").join("-");
-    debugger
+    const workItemTitleValueDashed = workItemTitleOriginalValue.split(" ").join("-");
+    const areaValueDashed = areaOriginalValue.split(" ").join("-");
+
+    const branchNameValue = `topics/${typeOriginalValue}(${areaValueDashed})=${workItemTitleValueDashed}#${workItem01OriginalValue}`;
+    const commitMessageValue = `${typeOriginalValue}(${areaValueDashed}): ${workItemTitleOriginalValue}#${workItem01OriginalValue}`;
+    const pullRequestValue = commitMessageValue;
+
+    branchNameHTMLElement.value = branchNameValue;
+    commitMessageHTMLElement.value = commitMessageValue;
+    pullRequestTitleHTMLElement.value = pullRequestValue;
+}
+
+function copyBranchName() {
+    navigator.clipboard.writeText(branchNameHTMLElement.value)
+        .then(() => {
+            // add toaster
+        })
+        .catch(err => {
+            console.error("Failed to copy: ", err);
+        });
+}
+
+function copyCommitMessage() {
+    navigator.clipboard.writeText(commitMessageHTMLElement.value)
+        .then(() => {
+            // add toaster
+        })
+        .catch(err => {
+            console.error("Failed to copy: ", err);
+        });
+}
+
+function copyPullRequestTitle() {
+    navigator.clipboard.writeText(pullRequestTitleHTMLElement.value)
+        .then(() => {
+            // add toaster
+        })
+        .catch(err => {
+            console.error("Failed to copy: ", err);
+        });
 }
